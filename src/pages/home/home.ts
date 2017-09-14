83df28bcfefbccf17dd1067449711fb045d9a30f
simon.castaneda <simon.castaneda@linctik.co>
2017-09-14 10:36:30 -0500

    fin plantilla

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,18 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+public gaming: any;
   constructor(public navCtrl: NavController) {
 
   }
 
+  aparecer(){
+    console.log(this.gaming);
+    document.getElementById('oculto1').style.display = 'block';
+    
+  }
+  ocultar(){
+     document.getElementById('oculto1').style.display = 'none';
+  }
 }
+ 
